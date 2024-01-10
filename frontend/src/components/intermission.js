4149be327e9f299b357b618d0d8be19b8231fe26
Tobias Busch <tobias_busch@ymail.com>
2024-01-10 16:25:12 +0100

    added intermission panel

--- frontend/src/components/intermission.js
+++ frontend/src/components/intermission.js
@@ -4,65 +4,22 @@ import React, { useEffect, useRef, useState } from "react";
 import "./intermission.css";
 
 function Intermission({ socket, players, roundResults, image }) {
-  const [pointsAnimations, setPointsAnimations] = useState([]);
 
-  const animatePoints = () => {
-    const animations = [];
-
-    roundResults.forEach((result, index) => {
-      const pointsText = `${result.points} Pts from ${result.from}`;
-
-      animations.push(
-        setTimeout(() => {
-          setPointsAnimations((prevAnimations) => [
-            ...prevAnimations,
-            { username: result.player.username, pointsText },
-          ]);
-        }, index * 2000) // 2000ms (2 seconds) delay between animations
-      );
-    });
-
-    // Clear timeouts when component unmounts
-    return () => animations.forEach((animation) => clearTimeout(animation));
-  };
-
-  useEffect(() => {
-    // Trigger animation for point details after component mounts
-    animatePoints();
-
-    // Cleanup function to clear timeouts when component unmounts
-    return () => setPointsAnimations([]);
-  }, []);
 
   return (
     <div className="intermission-section">
+      <ul className="player-list">
+                                {players.map((player, index) => (
+                                    <li key={index} className="player-item intermission">
+                                        {player.isHost && '👑'} {player.username} - {player.score}
+                                    </li>
+                                ))}
+                            </ul>
       <h1 className="round-results">Round Results</h1>
       <h2>{roundResults[0].correctAnswer}</h2>
       <div className="game-image">
         <img className="game-image" src={image} alt="Game Screen" />
       </div>
-      <ul className="player-details">
-        {roundResults.map((result, index) => (
-          <li key={index} className="player-item">
-            <p>
-              <span className="point-details">
-                {result.player.username}
-              </span>
-            </p>
-            {pointsAnimations.map((animation, animationIndex) =>
-              animationIndex === index ? (
-                <div
-                  key={animationIndex}
-                  className="point-popup animate-points"
-                >
-                  +{animation.pointsText}
-                </div>
-              ) : null
-            )}
-          </li>
-        ))}
-      </ul>
-      {/* Add any additional content or actions you want during intermission */}
     </div>
   );
 }
